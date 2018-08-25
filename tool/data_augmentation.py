938fc0836864e4485b72e52f82e8bc06e07a237b
ping <843951249@qq.com>
2018-08-25 18:44:12 +0800

    process_jpeg

---
+++ tool/data_augmentation.py
@@ -0,0 +1,84 @@
+# -*- coding:utf-8 -*-
+
+from PIL import Image, ImageEnhance, ImageOps, ImageFile
+import numpy as np
+import random
+import threading
+import os
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+class DataAugmentation:
+    """
+    包含数据增强的八种方式
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def openImage(image):
+        return Image.open(image, mode="r")
+
+    @staticmethod
+    def randomColor(image):
+        """
+        对图像进行颜色抖动
+        :param image: PIL的图像image
+        :return: 有颜色色差的图像image
+        """
+        random_factor = np.random.randint(0, 31) / 10.  # 随机因子
+        color_image = ImageEnhance.Color(
+            image).enhance(random_factor)  # 调整图像的饱和度
+        random_factor = np.random.randint(10, 21) / 10.  # 随机因子
+        brightness_image = ImageEnhance.Brightness(
+            color_image).enhance(random_factor)  # 调整图像的亮度
+        random_factor = np.random.randint(10, 21) / 10.  # 随机因1子
+        contrast_image = ImageEnhance.Contrast(
+            brightness_image).enhance(random_factor)  # 调整图像对比度
+        random_factor = np.random.randint(0, 31) / 10.  # 随机因子
+        # 调整图像锐度
+        return ImageEnhance.Sharpness(contrast_image).enhance(random_factor)
+
+    @staticmethod
+    def randomGaussian(image, mean=0.2, sigma=0.3):
+        """
+         对图像进行高斯噪声处理
+        :param image:
+        :return:
+        """
+
+        def gaussianNoisy(im, mean=0.2, sigma=0.3):
+            """
+            对图像做高斯噪音处理
+            :param im: 单通道图像
+            :param mean: 偏移量
+            :param sigma: 标准差
+            :return:
+            """
+            for _i in range(len(im)):
+                im[_i] += random.gauss(mean, sigma)
+            return im
+
+        # 将图像转化成数组
+        img = np.asarray(image)
+        img.flags.writeable = True  # 将数组改为读写模式
+        width, height = img.shape[:2]
+        img_r = gaussianNoisy(img[:, :, 0].flatten(), mean, sigma)
+        img_g = gaussianNoisy(img[:, :, 1].flatten(), mean, sigma)
+        img_b = gaussianNoisy(img[:, :, 2].flatten(), mean, sigma)
+        img[:, :, 0] = img_r.reshape([width, height])
+        img[:, :, 1] = img_g.reshape([width, height])
+        img[:, :, 2] = img_b.reshape([width, height])
+        return Image.fromarray(np.uint8(img))
+
+    @staticmethod
+    def saveImage(image, path):
+        image.save(path)
+
+
+
---
+++ tool/dataset.py
@@ -0,0 +1,68 @@
+import os
+import re
+import torch
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+
+def combine(path):
+    #label_file_path:label文件的路径
+    a = []
+    pattern = re.compile(r'\s\d{1,3}')
+    with open(path, 'r') as f:
+        line = f.readlines()
+        for i in range(len(line)):
+            match = pattern.findall(line[i])
+            match = list(map(lambda x: int(x), match))
+            zeros = [0]*(5-len(match))
+            match = match + zeros
+            a.append(match)
+    return a
+
+
+class NPSET(Dataset):
+
+    def __init__(self, root, data_transform=None, train=False):
+        self.picroot = root
+        self.data_transform = data_transform
+        if not os.path.exists(self.picroot):
+            raise RuntimeError('{} doesnot exists'.format(self.picroot))
+        if train:
+            jpegs_path = os.path.join(self.picroot, 'process_train_jpeg/')
+            label_path = os.path.join(self.picroot, 'labels/training_label')
+        else:
+            jpegs_path = os.path.join(self.picroot, 'process_test_jpeg/')
+            label_path = os.path.join(self.picroot, 'labels/test_label')
+        imgs = []
+        a = 0
+        with open(label_path, 'r') as file:
+            for line in file.readlines():
+                jpeg_name = line.split(' ')[0]+'.jpeg'
+                jpeg_path = os.path.join(jpegs_path, jpeg_name)
+                image = Image.open(jpeg_path).convert('RGB')
+                imgs.append(image)
+            a = len(file.readlines())
+        self.dataset = imgs
+        self.labels = combine(label_path)
+        self.len = a
+
+
+    def code_to_vec(self, p, code):
+        def char_to_vec(c):
+            y = np.zeros((374,))
+            y[c] = 1.0
+            return y
+        c = np.vstack([char_to_vec(c) for c in code])
+        return c.flatten()
+
+    def __getitem__(self, index):
+        label, img = self.labels[index], self.dataset[index]
+        if self.data_transform is not None:
+            img = self.data_transform(img)
+        labelarray = self.code_to_vec(1, label)
+        return (img, labelarray)
+
+    def __len__(self):
+        return self.len
+
+
